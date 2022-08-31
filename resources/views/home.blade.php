e41f8ecb0e18dcd53d58d12fc904d5774d0080bf
dbonafin <demianbonafina@gmail.com>
2022-08-31 17:12:29 +0200

    confirm alert for comic book delete (bonus)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,5 +15,9 @@
             You can check our bestsellers <br>
             Or create your own comic book!
         </h2>
+
+        <section class="home-bg">
+            <img src="https://pbs.twimg.com/media/Dpfopk-XgAENPuM.jpg" alt="dc comics">
+        </section>
     </div>
 @endsection
